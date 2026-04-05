45839f8eb5d3b17a3a42d9280401647ff42178d7
Rhitam Bhuiya <rhitambhuiya@yahoo.com>
2026-04-06 01:15:55 +0530

    Add files via upload

---
+++ Computer_Networks/Error_Detection/Hamming.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countParity(int pos, vector<int> &data)
+{
+    int start = pos - 1;
+    int count = 0;
+    while(start < data.size())
+    {   
+        for(int i = start; i < min((int)data.size(), i + pos); i++)
+        {
+            if(data[i] != -1)
+                count += data[i];
+        }
+        start += pos * 2;
+    }
+    return count % 2;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the bit stream: ";
+    cin >> s;
+    int d = s.length();
+    int r = 0;
+    while ((1 << r) < (r + d + 1))
+        r += 1;
+    vector<int> data(r + d, -1);
+    reverse(s.begin(), s.end());
+
+    int j = 0;
+    for(int i = 0; i < r + d; i++)
+    {
+        if((i & (i + 1)) == 0) // the indices which are power of 2 are redundant bits, if (i + 1) is a power of 2, then ((i + 1) & i) == 0
+            continue;
+        data[i] = s[j++] - '0';
+    }
+
+    for(int i = 0; i < r; i++)
+    {
+        data[(1 << i) - 1] = countParity(1 << i, data);
+    }
+
+    reverse(data.begin(), data.end());
+    cout << endl << "Hamming Coded Bitstream: ";
+    for(int d: data)
+        cout << d;
+
+    string received;
+    cout << endl << "Enter the data received at receiver's end: ";
+    cin >> received;
+
+    reverse(received.begin(), received.end());
+    reverse(data.begin(), data.end());
+    j = 0;
+    for(int i = 0; i < r + d; i++)
+    {
+        if((i & (i + 1)) == 0)
+            continue;
+        data[i] = received[j++] - '0';
+    }
+
+    reverse(data.begin(), data.end());
+    cout << endl << "Bitstream received by the user: ";
+    for(int d: data)
+        cout << d;
+
+    int error_bit = 0;
+    for(int i = 0; i < r; i++)
+    {
+        int p = countParity(1 << i, data);
+        error_bit += (1 << i) * p;
+    }
+
+    if(error_bit == 0)
+        cout << endl << "No error in transmission";
+    else
+        cout << endl << "Error in bit " << error_bit;
+}
