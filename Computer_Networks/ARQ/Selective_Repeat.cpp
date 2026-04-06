03652b3a9fa5d3de7ca9b4379444432b0c70ceb6
Rhitam Bhuiya <rhitambhuiya@yahoo.com>
2026-04-06 23:29:54 +0530

    Add files via upload

---
+++ Computer_Networks/ARQ/Selective_Repeat.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isLost()
+{
+    return ((double)rand()) / RAND_MAX < 0.5;
+}
+
+void selectiveRepeatARQ(int frames, int window)
+{
+	vector<bool> ack(frames, false);
+	int base = 0;
+	while(base < frames)
+	{
+		for(int i = base; i < min(base + window, frames); i++)
+		{
+			if(!ack[i]){
+				cout << "Sending frame " << i << "...";
+				if (isLost())
+				{
+					cout << "Frame lost! Resending later\n";
+					this_thread::sleep_for(chrono::seconds(2));
+				}
+				else
+				{
+					cout << "Ack received\n";
+					ack[i] = true;
+					this_thread::sleep_for(chrono::milliseconds(500));
+				}
+				
+			}
+		}
+		while (base < frames && ack[base])
+			base += 1;
+	}
+}
+
+int main()
+{
+	srand(time(0));
+	int frames, window;
+	cout << "Enter the number of frames to transmit and window size: ";
+	cin >> frames >> window;
+	selectiveRepeatARQ(frames, window);
+}
---
+++ Computer_Networks/ARQ/Stop_Wait.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isLost()
+{
+    return ((double)rand()) / RAND_MAX < 0.5;
+}
+
+void stopAndWaitARQ(int frames)
+{
+	for(int i = 1; i <= frames; i++)
+	{
+		cout << endl << "Sending frame " << i << "..." << endl;
+		if(isLost())
+		{
+			this_thread::sleep_for(chrono::seconds(2));
+			cout << endl << "Frame lost! Resending..." << endl;
+			i -= 1;
+		}
+		else
+		{
+			this_thread::sleep_for(chrono::milliseconds(500));
+			cout << endl << "Acknowledgement Received" << endl;
+		}
+	}
+}
+
+int main()
+{
+	srand(time(0));
+	int frames;
+	cout << "Enter the number of frames to transmit: ";
+	cin >> frames;
+	stopAndWaitARQ(frames);
+}
